7b3d7ca5051c07f208052944a5051b255f3ed4b8
johncarlo03 <120325948+johncarlo03@users.noreply.github.com>
2025-12-12 17:17:35 +0800

    logs fix

--- backend/logout.php
+++ backend/logout.php
@@ -6,20 +6,20 @@ $user_name = $_SESSION['name'];
 $user_id = $_SESSION['id'];
 $user_role = $_SESSION['roles'];
 
-if ($user['roles'] === "staff") {
-    $log_stmt->execute([$_SESSION['id'], "Staff " . $_SESSION['name'] . " has logged in."]);
-    header("Location: staff/sales.php");
-    exit();
-}
+// if ($user['roles'] === "staff") {
+//     $log_stmt->execute([$_SESSION['id'], "Staff has logged in."]);
+//     header("Location: staff/sales.php");
+//     exit();
+// }
 $log_sql = "INSERT INTO audit_logs (user_id, action, date_time) VALUES (?, ?, NOW())";
 $log_stmt = $conn->prepare($log_sql);
 
 if ($user_role === "admin") {
-    $log_stmt->execute([$user_id, "Administrator " . $user_name . " logged out."]);
+    $log_stmt->execute([$user_id, "Administrator logged out."]);
 }
 
 if ($user_role === "staff") {
-    $log_stmt->execute([$user_id, "Staff " . $user_name . " logged out."]);
+    $log_stmt->execute([$user_id, "Staff logged out."]);
 }
 
 
